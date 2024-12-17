88d21e696221014a2832cef1599e9ac922999532
Kanish Mittal <mittalkanish21@gmail.com>
2024-12-18 01:01:46 +0530

    learning

---
+++ video 54/new.js
@@ -0,0 +1,6 @@
+// to check node ->   node "D:\Coding\Web Developement\Learning-Web-Dev\video 54\new.js"
+
+console.log("Hello World !")
+console.log("Code is running")
+console.log("Code is also running")
+console.log("Code is looking like a wow")
---
+++ video 54/script.js
@@ -0,0 +1,18 @@
+alert("Hello World");
+
+console.log("Code is running")
+console.log("Code is also running")
+console.log("Code is looking like a wow")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to leave💀")
+
+if(isTrue) console.log("Left")
+    
+else console.log("Didn't left")
+    
+console.log("Your number is: " + a)
+
+document.title = "Made by JavaScript"
+
+document.body.style.backgroundColor = "yellow"
