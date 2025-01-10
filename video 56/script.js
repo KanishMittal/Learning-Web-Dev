db64633121695e01359964ac2fc5e72c41a02284
Kanish Mittal <mittalkanish21@gmail.com>
2025-01-10 23:36:53 +0530

    learning

---
+++ video 56/script.js
@@ -0,0 +1,14 @@
+console.log("Hello, i am a conditional tutorial")
+
+let age = 16;
+let grace = 2;
+
+if((age+grace)>=18){
+    console.log("You can drive");
+}
+
+else{
+    console.log("You cannot drive");
+}
+
+console.log(age**grace)
